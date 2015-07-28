c3939b2e1f5ec5a5d5cdd12b63708d98edc6b0dc
Michele Randi <michele.randi@gmail.com>
2015-07-28 16:28:15 +0200

    add readme info

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # zalando-cheat-sheet-generator
+
+To use this generator:
+
+* define a [schema.json](schema/example.json)
+* GET on  https://stups.io/zalando-cheat-sheet-generator/?schema=YOUR_SCHEMA_URL
+
+Example:
+
+[https://stups.io/zalando-cheat-sheet-generator/?schema=https://raw.githubusercontent.com/zalando-stups/zalando-cheat-sheet-generator/gh-pages/schema/example.json](https://stups.io/zalando-cheat-sheet-generator/?schema=https://raw.githubusercontent.com/zalando-stups/zalando-cheat-sheet-generator/gh-pages/schema/example.json)
--- js/main.js
+++ js/main.js
@@ -1,53 +1,49 @@
 "use strict";
 
 var data = [],
-  template,
-  BASE_URL = getUrlParameter('schema');
+    template,
+    BASE_URL = getUrlParameter('schema');
 
 function getUrlParameter(sParam) {
-  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-    sURLVariables = sPageURL.split('&'),
-    sParameterName,
-    i;
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
-  for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split('=');
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
 
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1] === undefined ? true : sParameterName[1];
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
     }
-  }
 };
 
 function renderElements(elementValues) {
-  var $hz = $('p.aws').first();
-  $hz.children().remove();
+    var $hz = $('p.zcs').first();
+    $hz.children().remove();
 
-  elementValues.forEach(function(elementValue) {
-    console.log(elementValue);
-    $hz.append(template(elementValue));
-  });
+    elementValues.forEach(function (elementValue) {
+        console.log(elementValue);
+        $hz.append(template(elementValue));
+    });
 
 }
 
 
 function getData() {
-  $.getJSON(BASE_URL)
-    .then(function(json) {
-      data.push(json);
-      if (data.length > 400) {
-        data.splice(0, 1);
-      }
-      renderElements(data[data.length - 1].elements);
-    });
+    $.getJSON(BASE_URL)
+        .then(function (json) {
+            data.push(json);
+            renderElements(data[data.length - 1].elements);
+        });
 
 
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-  template = Handlebars.compile($('#aws-template').html());
+    template = Handlebars.compile($('#zcs-template').html());
 
-  getData();
-  //setInterval(getData, 5000);
+    getData();
 });
